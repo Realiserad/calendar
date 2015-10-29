0f44059b33ba07a6735a5117db14056325d76d46
Sai Man Wong <smwong@kth.se>
2015-10-29 13:47:38 +0100

    Bugfix

--- test.cpp
+++ test.cpp
@@ -5,7 +5,11 @@
 #include <assert.h>
 
 int main() {
-	lab2::Gregorian g(1993, 2, 16);
+	lab2::Gregorian g(1900, 1, 1);
 	lab2::Julian j(g);
 	std::cout << j << std::endl;
+
+    lab2::Julian x(1899, 12, 20);
+    lab2::Gregorian y(x);
+    std::cout << y << std::endl;
 }
